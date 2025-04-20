2014ee4ff0162f82129d6dea212e145707602491
Ugege Daniel <ugege62@gmail.com>
2025-04-20 01:04:43 +0100

    adjusted vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// Fix __dirname for ESM
-import { fileURLToPath } from 'url'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
